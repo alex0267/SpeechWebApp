69582202ac67fc34e45109f613b11ccff32bdf93
Thomas Binétruy <tbinetruy@gmail.com>
2020-12-08 13:25:43 +0100

    integrated voice recorder component

--- web-app/js/index.js
+++ web-app/js/index.js
@@ -1,10 +1,60 @@
-//import {Recorder} from 'react-voice-recorder'
-//import 'react-voice-recorder/dist/index.css'
-
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Foo = () => <div>Hello</div>;
+import {Recorder} from 'react-voice-recorder';
+import 'react-voice-recorder/dist/index.css';
 
-ReactDOM.render(<Foo />, document.getElementById("root"));
+class VoiceRecorder extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            audioDetails: {
+                url: null,
+                blob: null,
+                chunks: null,
+                duration: {
+                    h: 0,
+                    m: 0,
+                    s: 0
+                }
+            }
+        };
+    }
+    handleAudioStop(data){
+        console.log(data);
+        this.setState({ audioDetails: data });
+    }
+    handleAudioUpload(file) {
+        console.log(file);
+    }
+    handleRest() {
+        const reset = {
+            url: null,
+            blob: null,
+            chunks: null,
+            duration: {
+                h: 0,
+                m: 0,
+                s: 0
+            }
+        };
+        this.setState({ audioDetails: reset });
+    }
+    render() {
+        return (
+            <Recorder
+                record={true}
+                title={"New recording"}
+                audioURL={this.state.audioDetails.url}
+                showUIAudio
+                handleAudioStop={data => this.handleAudioStop(data)}
+                handleAudioUpload={data => this.handleAudioUpload(data)}
+                handleRest={() => this.handleRest()}
+            />
+        );
+    }
+}
+
+
+ReactDOM.render(<VoiceRecorder />, document.getElementById("root"));
 
