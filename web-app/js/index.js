bceb59644307bc59c7a889f52d039e1b0ffa32d7
Thomas Binétruy <tbinetruy@gmail.com>
2020-12-08 13:09:21 +0100

    created webapp frontend boilerplate

---
+++ web-app/js/index.js
@@ -0,0 +1,10 @@
+//import {Recorder} from 'react-voice-recorder'
+//import 'react-voice-recorder/dist/index.css'
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Foo = () => <div>Hello</div>;
+
+ReactDOM.render(<Foo />, document.getElementById("root"));
+
