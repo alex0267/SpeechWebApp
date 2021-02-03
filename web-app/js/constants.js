7b721581eb4e4f44710620921bfa8a1e9327688e
Mikael Koutero <mkoutero@wewyse.com>
2021-02-03 16:34:44 +0100

    testing loopback

--- web-app/js/constants.js
+++ web-app/js/constants.js
@@ -1,1 +1,2 @@
-export const apiHost = `${window.location.protocol}//${window.location.hostname}:8081`;
+// export const apiHost = `${window.location.protocol}//${window.location.hostname}:8081`;
+export const apiHost = `http://localhost:8081`;
