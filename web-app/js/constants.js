bdb31df0f2f72f39f0ca914be36fc3510bf6978f
Mikael Koutero <mkoutero@wewyse.com>
2021-02-03 17:15:07 +0100

    update front host direction

--- web-app/js/constants.js
+++ web-app/js/constants.js
@@ -1,2 +1,1 @@
-// export const apiHost = `${window.location.protocol}//${window.location.hostname}:8081`;
-export const apiHost = `http://localhost:8081`;
+export const apiHost = `${window.location.protocol}//${window.location.hostname}`;
