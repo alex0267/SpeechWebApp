3bcd5fdd89b1b7f6803d23927811fce15c6772e4
Thomas Binétruy <tbinetruy@gmail.com>
2021-02-15 19:23:05 +0100

    use backend port from frontent only on localhost

--- web-app/js/constants.js
+++ web-app/js/constants.js
@@ -1,1 +1,10 @@
-export const apiHost = `${window.location.protocol}//${window.location.hostname}`;
+let isDev = window.location.hostname == "localhost";
+
+let apiHost = "";
+if (isDev) {
+    apiHost = `${window.location.protocol}//${window.location.hostname}:8081`;
+} else {
+    apiHost = `${window.location.protocol}//${window.location.hostname}`;
+}
+
+export {apiHost};
